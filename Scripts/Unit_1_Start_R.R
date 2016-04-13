cc0398f86a77415f7b33567c309b4718c8c4838c
Coding4Sec <randbedingung@hotmail.de>
2016-04-13 06:20:21 +0200

    Initial Commit. Starting Course and generate structure

---
+++ Scripts/Unit_1_Start_R.R
@@ -0,0 +1,7 @@
+# If you downloaded and installed R in a 
+# location other than the United States, 
+# you might encounter some formating issues 
+# later in this class due to language differences. 
+# To fix this, you will need to type in your R console:
+  
+Sys.setlocale("LC_ALL", "C")
